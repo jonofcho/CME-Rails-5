77c38f5b13d9b3f27618c483f37906dbbccaf6e2
jonathan Cho <jonathancho@jonathans-MacBook-Pro.local>
2016-10-25 23:25:48 -0700

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  before_action :authenticate_user! , except: [:index,:finder,:dreamer]
+  before_filter :set_cache_buster
+  protect_from_forgery
+  def after_sign_in_path_for(resource)
+    current_user
+  end
+
+
+
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, :only => [:create]
+
+  def index
+
+  end
+
+  protected
+    def after_sign_up_path_for(resource)
+      second_path
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :date_of_birth, :email, :password, :avatar) }
+    end
+end
